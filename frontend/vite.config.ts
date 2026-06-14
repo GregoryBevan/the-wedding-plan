6b3a979e18c3b3787f59076fa829f7d5846790ba
Gregory Bevan <gregory_bevan@hotmail.com>
2026-06-14 21:32:41 +0200

    feat(#9): 🎉 Separate frontend into backoffice and public applications

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        public: resolve(__dirname, 'public/index.html'),
+        backoffice: resolve(__dirname, 'backoffice/index.html'),
+      },
+    },
+  },
 })
